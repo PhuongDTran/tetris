424cab2c7059e9622ba69c5b6774179b4ab0bfc8
Phuong Tran <ptran816@uw.edu>
2019-08-26 12:15:21 -0700

    the look set up

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cell from './Cell'
 
+import {StyledStage} from './styles/StyledStage'
+
 function Stage({ stage }) {
   return (
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
        {/* a row is an element in stage. each row is an array containing two-element arrays */}
       {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-    </div>
+    </StyledStage>
   )
 }
 

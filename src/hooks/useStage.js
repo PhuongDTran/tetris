efbbd3bd8903eed03a2334b324a68494308d00f2
Phuong Tran <ptran816@uw.edu>
2019-08-26 14:21:25 -0700

    usePlayer, useStage hooks

---
+++ src/hooks/useStage.js
@@ -0,0 +1,8 @@
+import {useState} from 'react'
+import {createStage} from '../gameHelpers'
+
+export function useStage() {
+  const [stage, setStage] = useState(createStage());
+
+  return [stage, setStage];
+}
